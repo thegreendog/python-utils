fffa64aba97f0f3141b7d76a3323544dcc7503d8
Josu Moreno <josumoreno@gmail.com>
2018-10-03 16:46:44 +0200

    Add django healthcheck middleware adn bump to 0.0.3

---
+++ python_utils/django/middleware/healthcheck.py
@@ -0,0 +1,56 @@
+"""Middleware to check health and readyness. Thank you ianlewis"""
+import logging
+
+from django.http import HttpResponse, HttpResponseServerError
+
+logger = logging.getLogger("health")
+
+
+class HealthCheckMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == "GET":
+            if request.path == "/health":
+                return self.health(request)
+            elif request.path == "/readiness":
+                return self.readiness(request)
+        return self.get_response(request)
+
+    def health(self, request):
+        """Returns that the server is alive."""
+        return HttpResponse("OK")
+
+    def readiness(self, request):
+        """Returns that the databases and caches are ready"""
+        # Connect to each database and do a generic standard SQL query that doesn't
+        # write any data and doesn't depend on any tables being present.
+        try:
+            from django.db import connections
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute("SELECT 1;")
+                row = cursor.fetchone()
+                if row is None:
+                    return HttpResponseServerError("db: invalid response")
+        except Exception as e:
+            logger.exception(e)
+            return HttpResponseServerError("db: cannot connect to database.")
+
+        # Call get_stats() to connect to each memcached instance and get it's stats.
+        # This can effectively check if each is online.
+        try:
+            from django.core.cache import caches
+            from django.core.cache.backends.memcached import BaseMemcachedCache
+            for cache in caches.all():
+                if isinstance(cache, BaseMemcachedCache):
+                    stats = cache._cache.get_stats()
+                    if len(stats) != len(cache._servers):
+                        return HttpResponseServerError("cache: cannot connect to cache.")
+        except Exception as e:
+            logger.exception(e)
+            return HttpResponseServerError("cache: cannot connect to cache.")
+
+        return HttpResponse("OK")
